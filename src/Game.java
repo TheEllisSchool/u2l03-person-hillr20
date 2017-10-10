c1392f17836d7e10b38ee089ed797ba339f09cd1
lombardim <lombardim@Lombardi-12MF3Q.ellis.int>
2017-10-10 08:52:11 -0400

    "insert game name here"

--- src/Game.java
+++ src/Game.java
@@ -3,7 +3,7 @@ import java.awt.*;
 public class Game extends DrawableAdapter {
 	
 	static Game ga = new Game();
-	static GameBoard gb = new GameBoard(ga, "Lines and Shapes");
+	static GameBoard gb = new GameBoard(ga, "Insert Game Name Here");
 	
 	public static void main(String[] args) {
 		showGameBoard(gb);
